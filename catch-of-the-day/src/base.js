6dd78a94423f652517f9c6191ce7c2ba9461972c
Ken Edwards <ken@meancode.com>
2017-06-28 23:53:48 -0400

    My work on Catch Of The Day

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBb0flqDDowKVV473JEIjQt1r1ybUQc5N8",
+    authDomain: "catch-of-the-day-meancode.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-meancode.firebaseio.com",
+});
+
+export default base;
--- catch-of-the-day/src/helpers.js
+++ catch-of-the-day/src/helpers.js
@@ -16,9 +16,11 @@ export function slugify(text) {
 }
 
 export function getFunName() {
+  const colors = ['blue', 'green', 'red', 'white', 'yellow', 'black', 'gray', 'orange', 'purple', 'pink', 'brown'];
+
   const adjectives = ['adorable', 'beautiful', 'clean', 'drab', 'elegant', 'fancy', 'glamorous', 'handsome', 'long', 'magnificent', 'old-fashioned', 'plain', 'quaint', 'sparkling', 'ugliest', 'unsightly', 'angry', 'bewildered', 'clumsy', 'defeated', 'embarrassed', 'fierce', 'grumpy', 'helpless', 'itchy', 'jealous', 'lazy', 'mysterious', 'nervous', 'obnoxious', 'panicky', 'repulsive', 'scary', 'thoughtless', 'uptight', 'worried'];
 
   const nouns = ['women', 'men', 'children', 'teeth', 'feet', 'people', 'leaves', 'mice', 'geese', 'halves', 'knives', 'wives', 'lives', 'elves', 'loaves', 'potatoes', 'tomatoes', 'cacti', 'foci', 'fungi', 'nuclei', 'syllabuses', 'analyses', 'diagnoses', 'oases', 'theses', 'crises', 'phenomena', 'criteria', 'data'];
 
-  return `${rando(adjectives)}-${rando(adjectives)}-${rando(nouns)}`;
+  return `${rando(colors)}-${rando(adjectives)}-${rando(nouns)}`;
 }
